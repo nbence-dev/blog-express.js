cba7921d7fc695219169f0c787cca984aede9f53
Nicholas Bence <nicholasbence12@gmail.com>
2026-02-12 19:45:06 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Active on port ${port}`);
+});
